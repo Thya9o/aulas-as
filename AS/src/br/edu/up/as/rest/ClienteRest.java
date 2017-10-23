54975864d7c7c8f4b4f3d4efdeeb2560cc4d01d6
ctup <ctup@DESKTOP-8JG1FTB>
2017-10-23 21:02:32 -0200

    dev
    - rest

--- AS/src/br/edu/up/as/rest/ClienteRest.java
+++ AS/src/br/edu/up/as/rest/ClienteRest.java
@@ -1,5 +1,61 @@
 package br.edu.up.as.rest;
 
+import java.util.List;
+import java.util.ArrayList;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import br.edu.up.as.entidade.Cliente;
+import br.edu.up.as.service.clienteService;
+import br.edu.up.as.service.ServiceException;
+
+@Path("/detalhe-cliente")
 public class ClienteRest {
 
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void cadastrarCliente(Cliente o) {
+		try {
+			new clienteService().salvar(o);
+		}catch (ServiceException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Cliente buscarSuccess(Integer id) {
+		Cliente o = new clienteService().buscar(id);
+		return o;
+	}
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void alterarCliente(Cliente o) {
+		try {
+			new clienteService().alterar(o);
+		}catch (ServiceException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public ArrayList<Cliente> listarClientes() {
+		List<Cliente> lista = new clienteService().listar();
+		return new ArrayList<>(lista);
+	}
+
+
+	@DELETE
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void deletarCliente(Cliente o) {
+		new clienteService().excluir(o);
+	}
 }
--- AS/src/br/edu/up/as/rest/ServicoRest.java
+++ AS/src/br/edu/up/as/rest/ServicoRest.java
@@ -1,5 +1,61 @@
 package br.edu.up.as.rest;
 
+import java.util.List;
+import java.util.ArrayList;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.core.MediaType;
+
+import br.edu.up.as.entidade.Servico;
+import br.edu.up.as.service.servicoService;
+import br.edu.up.as.service.ServiceException;
+
+@Path("/detalhe-servico")
 public class ServicoRest {
 
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void cadastrarServico(Servico o) {
+		try {
+			new servicoService().salvar(o);
+		}catch (ServiceException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Servico buscarServico(Integer id) {
+		Servico o = new servicoService().buscar(id);
+		return o;
+	}
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void alterarServico(Servico o) {
+		try {
+			new servicoService().alterar(o);
+		}catch (ServiceException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public ArrayList<Servico> listarServicos() {
+		List<Servico> lista = new servicoService().listar();
+		return new ArrayList<>(lista);
+	}
+
+
+	@DELETE
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void deletarServico(Servico o) {
+		new servicoService().excluir(o);
+	}
 }
